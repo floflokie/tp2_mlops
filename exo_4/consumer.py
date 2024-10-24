b41679c5eb3f173e5e26fef94660dc562c5ec498
Florine Kieraga <florine.kieraga@epita.fr>
2024-10-24 16:41:23 +0200

    [fix] exo4

--- exo_4/consumer.py
+++ exo_4/consumer.py
@@ -3,7 +3,7 @@ from model_utils import load_model, make_prediction
 import json
 
 model = load_model()
-consumer = KafkaConsumer('prediction_florine_kieraga', bootstrap_servers='51.38.185.58:9092')
+consumer = KafkaConsumer('prediction_florine_kieraga', bootstrap_servers='51.38.185.58:9092', group_id='prediction_group')
 for message in consumer:
     converted_message = message.value.decode()
     message_load = json.loads(converted_message)
