09880c2871748041f262e199d773f7e9f5f4c04a
Florine Kieraga <florine.kieraga@epita.fr>
2024-10-24 16:28:42 +0200

    [feat] exo4

---
+++ exo_4/consumer.py
@@ -0,0 +1,12 @@
+from kafka import KafkaConsumer
+from model_utils import load_model, make_prediction
+import json
+
+model = load_model()
+consumer = KafkaConsumer('prediction_florine_kieraga', bootstrap_servers='51.38.185.58:9092')
+for message in consumer:
+    converted_message = message.value.decode()
+    message_load = json.loads(converted_message)
+    data = message_load['data']
+    prediction = make_prediction(size=data['size'], nb_rooms=data['nb_rooms'], garden=data['garden'], model=model)
+    print("Prédiction : ", prediction)
